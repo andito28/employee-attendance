b7753609997434e4b3755f2ae7e7252aaf3b1a4d
andito28 <andito763@gmail.com>
2024-06-05 13:48:23 +0800

    update employee algo

--- app/Algorithms/Employee/EmployeeAlgo.php
+++ app/Algorithms/Employee/EmployeeAlgo.php
@@ -7,13 +7,11 @@ use Illuminate\Http\Request;
 use App\Models\Employee\User;
 use App\Models\Employee\Sibling;
 use App\Models\Employee\Employee;
-use App\Models\Employee\Parental;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Support\Facades\DB;
 use App\Services\Constant\RoleUser;
 use App\Models\Employee\Resignation;
 use Illuminate\Support\Facades\Auth;
-use Illuminate\Support\Facades\Hash;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Facades\Storage;
 use App\Services\Constant\StatusEmployee;
@@ -129,8 +127,8 @@ class EmployeeAlgo
 
     /** --- SUB FUNCTIONS --- */
 
-    private function  checkExistingEmployeeAndResignation($request){
-
+    private function  checkExistingEmployeeAndResignation($request)
+    {
         $existingEmployee = Employee::where('statusId',StatusEmployee::ACTIVE_ID)
         ->whereHas('user', function($query) use ($request) {
             $query->where('email', $request->email);
@@ -157,8 +155,8 @@ class EmployeeAlgo
     }
 
 
-    private function createEmployee($request,$createdBy){
-
+    private function createEmployee($request,$createdBy)
+    {
         $filePath = $this->savePhoto($request);
 
         $dataInput = [
@@ -174,9 +172,9 @@ class EmployeeAlgo
 
         $employee = Employee::create($dataInput + $createdBy);
 
-        $this->createUser($request,$employee->id);
-        $this->createParentEmployee($request,$employee->id);
-        $this->createSiblingsEmployee($request,$employee->id,$createdBy);
+        $employee->saveUser($request->only(['email', 'password']));
+        $employee->saveParent($request);
+        $employee->saveSiblings($request,$createdBy);
 
         return $employee;
     }
@@ -187,45 +185,6 @@ class EmployeeAlgo
         $fileName = time() . '_' . $file->getClientOriginalName();
         $filePath = $file->storeAs('public/employee', $fileName);
         return $filePath;
-    }
-
-
-    private function createUser($request,$employeeId){
-        User::create([
-            'employeeId' => $employeeId,
-            'email' => $request->email,
-            'password' => Hash::make($request->password),
-            'roleId' => RoleUser::EMPLOYEE_ID
-        ]);
-    }
-
-
-    private function createParentEmployee($request,$employeeId){
-        Parental::create([
-            'employeeId' => $employeeId,
-            'fatherName' => $request->fatherName,
-            'fatherPhone' => $request->fatherPhone,
-            'fatherEmail' => $request->fatherEmail,
-            'motherName' => $request->motherName,
-            'motherPhone' => $request->motherPhone,
-            'motherEmail' => $request->motherEmail,
-        ]);
-    }
-
-
-    private function createSiblingsEmployee($request,$employeeId,$createdBy){
-
-        foreach($request->siblings as $value){
-
-            $dataInput = [
-                'employeeId' => $employeeId,
-                'name' => $value['name'],
-                'email' => $value['email'] ?? null,
-                'phone' => $value['phone'] ?? null
-            ];
-
-            Sibling::create($dataInput + $createdBy);
-        }
     }
 
 
@@ -241,8 +200,8 @@ class EmployeeAlgo
     }
 
 
-    private function updateEmployee($request){
-
+    private function updateEmployee($request)
+    {
         if($request->file('photo')){
             Storage::delete($this->employee->photo);
             $filePath = $this->savePhoto($request);
@@ -260,72 +219,14 @@ class EmployeeAlgo
         ];
 
         $this->employee->update($dataInput);
-
-        if($request->email){
-            $this->updateUser($this->employee->id,$request);
-        }
-
-        $this->updateParentEmployee($this->employee->id,$request);
-        $this->updateSiblingsEmployee($this->employee->id,$request);
+        $this->employee->saveUser($request->only(['email']));
+        $this->employee->saveParent($request);
+        $this->employee->saveSiblings($request);
     }
 
 
-    private function updateUser($employeeId,$request){
-        User::where('employeeId', $employeeId)->update([
-            'email' => $request->email
-        ]);
-    }
-
-
-    private function updateParentEmployee($employeeId,$request){
-
-        Parental::where('employeeId', $employeeId)->update([
-            'fatherName' => $request->fatherName,
-            'fatherPhone' => $request->fatherPhone,
-            'fatherEmail' => $request->fatherEmail,
-            'motherName' => $request->motherName,
-            'motherPhone' => $request->motherPhone,
-            'motherEmail' => $request->motherEmail,
-        ]);
-
-    }
-
-
-    private function updateSiblingsEmployee($employeeId,$request){
-        $processedSiblings = [];
-
-        foreach ($request->siblings as $siblingData) {
-            $siblingId = $siblingData['id'] ?? null;
-
-            if ($siblingId) {
-                $sibling = Sibling::where('id',$siblingId)
-                ->where('employeeId',$employeeId)->first();
-                    if (!$sibling) {
-                        errEmployeeSiblingsGet(
-                            "employee ID:".$employeeId." & "."sibling ID:". $siblingData['id']
-                        );
-                    }
-            } else {
-                $sibling = new Sibling();
-            }
-
-            $sibling->employeeId = $employeeId;
-            $sibling->name = $siblingData['name'];
-            $sibling->email = $siblingData['email'] ?? null;
-            $sibling->phone = $siblingData['phone'] ?? null;
-            $sibling->save();
-
-            $processedSiblings[] = $sibling->id;
-        }
-
-        Sibling::where('employeeId', $employeeId)
-            ->whereNotIn('id', $processedSiblings)
-            ->delete();
-    }
-
-
-    private function savePromoteAdmin($employeeId){
-
+    private function savePromoteAdmin($employeeId)
+    {
         User::where('employeeId', $employeeId)->update([
             'roleId' => RoleUser::ADMINISTRATOR_ID
         ]);
--- app/Models/Employee/Employee.php
+++ app/Models/Employee/Employee.php
@@ -6,8 +6,10 @@ use App\Models\BaseModel;
 use App\Models\Employee\User;
 use App\Models\Employee\Sibling;
 use App\Models\Employee\Parental;
+use App\Services\Constant\RoleUser;
 use App\Models\Component\Department;
 use App\Models\Employee\Resignation;
+use Illuminate\Support\Facades\Hash;
 use App\Models\Component\CompanyOffice;
 use App\Parser\Employee\EmployeeParser;
 use App\Models\Employee\Traits\HasActivityEmployeeProperty;
@@ -83,8 +85,8 @@ class Employee extends BaseModel
 
      /** --- FUNCTIONS --- */
 
-    public function delete(){
-
+    public function delete()
+    {
         $this->user()->delete();
         $this->parental()->delete();
         $this->siblings()->delete();
@@ -94,7 +96,82 @@ class Employee extends BaseModel
 
     public function saveUser($data)
     {
+        $user = $this->user;
 
+        if($user) {
+            $user->update(['email' => $data['email']]);
+        } else {
+            User::create([
+                'employeeId' => $this->id,
+                'email' => $data['email'],
+                'password' => Hash::make($data['password']),
+                'roleId' => RoleUser::EMPLOYEE_ID
+            ]);
+        }
+    }
+
+
+    public function saveParent($data)
+    {
+        $parental = $this->parental;
+
+        $dataInput = [
+            'fatherName' => $data['fatherName'],
+            'fatherPhone' => $data['fatherPhone'],
+            'fatherEmail' => $data['fatherEmail'],
+            'motherName' => $data['motherName'],
+            'motherPhone' => $data['motherPhone'],
+            'motherEmail' => $data['motherEmail']
+        ];
+
+        if($parental){
+            $parental->update($dataInput);
+        }else{
+            $employeeId = ['employeeId' => $this->id];
+            Parental::create($dataInput + $employeeId);
+        }
+    }
+
+
+    public function saveSiblings($data,$createdBy = null)
+    {
+        $siblings = $this->siblings;
+
+            $processedSiblings = [];
+            foreach ($data['siblings'] as $siblingData) {
+                $siblingId = $siblingData['id'] ?? null;
+
+                if ($siblingId)
+                {
+                    $sibling = Sibling::where('id',$siblingId)
+                    ->where('employeeId',$this->id)->first();
+                        if (!$sibling)
+                        {
+                            errEmployeeSiblingsGet(
+                                "employee ID:".$this->id." & "."sibling ID:". $siblingData['id']
+                            );
+                        }
+                } else {
+                    $sibling = new Sibling();
+                }
+
+                if($createdBy){
+                    $sibling->createdBy = $createdBy['createdBy'];
+                    $sibling->createdByName = $createdBy['createdByName'];
+                }
+
+                $sibling->employeeId = $this->id;
+                $sibling->name = $siblingData['name'];
+                $sibling->email = $siblingData['email'] ?? null;
+                $sibling->phone = $siblingData['phone'] ?? null;
+                $sibling->save();
+
+                $processedSiblings[] = $sibling->id;
+            }
+
+            Sibling::where('employeeId', $this->id)
+                ->whereNotIn('id', $processedSiblings)
+                ->delete();
     }
 
 }
