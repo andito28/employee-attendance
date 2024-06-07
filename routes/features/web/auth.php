27169cde3b45f03ac80f418e3d3b70677c5bb02a
andito28 <andito763@gmail.com>
2024-06-07 15:14:28 +0800

    update route auth

--- routes/features/web/auth.php
+++ routes/features/web/auth.php
@@ -4,10 +4,12 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\web\auth\AuthController;
 
 //AUTH
-Route::post('login', [AuthController::class, 'login']);
+Route::prefix("login")
+    ->group(function () {
 
+        Route::get("", [AuthController::class, "login"]);
 
-
+    });
 
 
 
