5eea09d608884a7865daa3eaf50e643caf360955
andito28 <andito763@gmail.com>
2024-06-19 14:59:39 +0800

    update generateTimesheetExcel

--- app/Services/Excel/generateTimesheetExcel.php
+++ app/Services/Excel/generateTimesheetExcel.php
@@ -2,12 +2,47 @@
 namespace App\Services\Excel;
 
 use App\Models\Attendance\Attendance;
-use Maatwebsite\Excel\Concerns\FromCollection;
+use Maatwebsite\Excel\Concerns\FromQuery;
+use App\Services\Constant\AttendanceStatus;
+use Maatwebsite\Excel\Concerns\WithMapping;
+use Maatwebsite\Excel\Concerns\WithHeadings;
 
-class GenerateTimesheetExcel implements FromCollection
+class GenerateTimesheetExcel implements FromQuery, WithMapping, WithHeadings
 {
-    public function collection()
+    /**
+     * @return \Illuminate\Database\Query\Builder
+     */
+    public function query()
     {
-        return Attendance::all();
+        return Attendance::query()->with('employee', 'shift');
+    }
+
+    /**
+     * @param mixed $attendance
+     * @return array
+     */
+    public function map($attendance): array
+    {
+        return [
+            $attendance->employee->name,
+            $attendance->clockIn,
+            $attendance->clockOut,
+            $attendance->shift->name,
+            AttendanceStatus::display($attendance->statusId),
+        ];
+    }
+
+    /**
+     * @return array
+     */
+    public function headings(): array
+    {
+        return [
+            'EMPLOYEE',
+            'CLOCK IN',
+            'CLOCK OUT',
+            'SHIFT',
+            'STATUS',
+        ];
     }
 }
